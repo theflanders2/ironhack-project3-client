da9f5c7534d42335909c894dc9cb9e5e1e9cbc84
theflanders2 <kenneth.flanders.ii@gmail.com>
2024-03-18 00:56:08 +0100

    initial add and configure of language.context, en-US.json and de-DE.json files

---
+++ src/context/language.context.jsx
@@ -0,0 +1,22 @@
+import { createContext, useState } from "react";
+
+const LanguageContext = createContext();
+
+function LanguageProviderWrapper(props) {
+  const [language, setLanguage] = useState("en-US");
+
+  const toggleLanguage = () => {
+    if (language === "en-US") {
+      setLanguage("de-DE");
+    } else {
+      setLanguage("en-US");
+    }
+  };
+  return (
+    <LanguageContext.Provider value={{language, toggleLanguage}}>
+      {props.children}
+    </LanguageContext.Provider>
+  );
+}
+
+export { LanguageContext, LanguageProviderWrapper };
