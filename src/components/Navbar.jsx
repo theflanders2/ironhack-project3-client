64715b25deef4550b2f1ff6dbd953bb95bc7d6a8
theflanders2 <kenneth.flanders.ii@gmail.com>
2024-03-18 00:58:48 +0100

    configure ThemeContext, LanguageContext, theme button, lanuage options

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,44 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { useContext } from "react";
+import { ThemeContext } from "../context/theme.context";
+import { LanguageContext } from "../context/language.context";
+
+import englishContent from "../en-US.json";
+import germanContent from "../de-DE.json";
 
 function Navbar() {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+  const { language, toggleLanguage } = useContext(LanguageContext);
+
   return (
-    <nav>
-      <Link to="/">
-        <button>Home</button>
-      </Link>
+    <nav className={`Navbar ${theme}`}>
+      <NavLink to="/">
+        <button>{language === "en-US" ? englishContent.navBar[0] : germanContent.navBar[0]}</button>
+      </NavLink>
 
-      <Link to="/signup">
-        <button>Sign Up</button>
-      </Link>
+      <NavLink to="/signup">
+        <button>{language === "en-US" ? englishContent.navBar[1] : germanContent.navBar[1]}</button>
+      </NavLink>
 
-      <Link to="/login">
-        <button>Log In</button>
-      </Link>
+      <NavLink to="/login">
+        <button>{language === "en-US" ? englishContent.navBar[2] : germanContent.navBar[2]}</button>
+      </NavLink>
 
-      <Link to="/profile">
-        <button>Profile</button>
-      </Link>
+      <NavLink to="/profile">
+        <button>{language === "en-US" ? englishContent.navBar[3] : germanContent.navBar[3]}</button>
+      </NavLink>
 
-      <Link to="/games">
-        <button>Games</button>
-      </Link>
+      <NavLink to="/games">
+        <button>{language === "en-US" ? englishContent.navBar[4] : germanContent.navBar[4]}</button>
+      </NavLink>
+
+      <button className="theme-btn" onClick={toggleTheme}>
+        {theme === "light" ? "dark " : "light "}
+      </button>
+      <select name="" id="" onChange={toggleLanguage}>
+        <option value="en-US">English</option>
+        <option value="de-DE">Deutsch</option>
+      </select>
     </nav>
   );
 }
