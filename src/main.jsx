41108ac8bc9353f94d83f5f62caaa51423d3a5f7
theflanders2 <kenneth.flanders.ii@gmail.com>
2024-03-18 01:31:42 +0100

    add Theme, Language and Auth Wrappers

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
-// import { AuthProviderWrapper } from "./context/auth.context";
+import { ThemeProviderWrapper } from "./context/theme.context.jsx";
+import { LanguageProviderWrapper } from "./context/language.context.jsx";
+import { AuthProviderWrapper } from "./context/auth.context";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      {/* <AuthProviderWrapper> */}
-        <App />
-      {/* </AuthProviderWrapper> */}
+      <ThemeProviderWrapper>
+        <LanguageProviderWrapper>
+          <AuthProviderWrapper>
+            <App />
+          </AuthProviderWrapper>
+        </LanguageProviderWrapper>
+      </ThemeProviderWrapper>
     </Router>
   </React.StrictMode>
 );
