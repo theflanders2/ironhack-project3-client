d626e6e7ad8fb4b1dd00426f6a80084bc4618854
theflanders2 <kenneth.flanders.ii@gmail.com>
2024-03-20 17:58:23 +0100

    add authService logic

--- src/context/auth.context.jsx
+++ src/context/auth.context.jsx
@@ -1,7 +1,6 @@
 import { createContext, useState, useEffect } from "react";
-import axios from "axios";
+import authService from "../services/auth.service";
 
-const API_URL = import.meta.env.VITE_DEPLOYMENT_SERVER_URL;
 const AuthContext = createContext();
 
 function AuthProviderWrapper(props) {
@@ -23,10 +22,11 @@ function AuthProviderWrapper(props) {
     // If the token exists in the localStorage
     if (storedToken) {
       // We must send the JWT token in the request's "Authorization" Headers
-      axios
+      /* axios // no longer needed due to authService
         .get(`${API_URL}/auth/verify`, {
           headers: { Authorization: `Bearer ${storedToken}` }
-        })
+        }) */
+      authService.verifyToken()
         .then((response) => {
           // If the server verifies that the JWT token is valid
           const user = response.data;
