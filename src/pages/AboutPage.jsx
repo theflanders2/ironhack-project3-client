0f779ec0ada7dd44ca4577c01a57b12a52b78c77
theflanders2 <kenneth.flanders.ii@gmail.com>
2024-03-30 05:57:45 +0100

    add about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,9 +1,14 @@
-
-
 function AboutPage() {
   return (
     <div className="AboutPage">
       <h1 className>About Gameodex</h1>
+      <p>
+        Gameodex is a site where you can create a personal index of Playstation
+        console games you have played, are currently playing or would like to
+        play. Along with keeping track of games, you can add games to the
+        database, as well as leave comments on games that other members of the
+        site can also see.
+      </p>
     </div>
   );
 }
