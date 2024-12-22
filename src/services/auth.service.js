1241684a43c41a795c750f8b94723d9fbdf622e9
theflanders2 <kenneth.flanders.ii@gmail.com>
2024-12-22 14:12:09 +0100

    :recycle: [authentication] added #api as prviate class field, updated header assignment to directly modify config.headers.Authorization, changed signUp, logIn and verifyToken methods to be async

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,42 +1,33 @@
 import axios from "axios";
 
 class AuthService {
+  #api; // use as private class field to ensure it can't be accessed outside class
+
   constructor() {
-    // Create a new instance of axios with a custom configuration
-    this.api = axios.create({
+    this.#api = axios.create({
       baseURL: import.meta.env.VITE_DEPLOYMENT_SERVER_URL || "http://localhost:5005",
     });
-    // Automatically set JWT token in the headers for every request
-    this.api.interceptors.request.use((config) => {
-      // interceptor will execute before request is sent
-      // Retrieve the JWT token from the local storage
+    
+    this.#api.interceptors.request.use((config) => {
       const storedToken = localStorage.getItem("authToken");
-
       if (storedToken) {
-        config.headers = { Authorization: `Bearer ${storedToken}` };
+        config.headers.Authorization = `Bearer ${storedToken}`;
       }
-
       return config;
     });
   }
 
-  signUp = (requestBody) => {
-    // same as
-    // return axios.post("http://localhost:5005/auth/signup");
-    return this.api.post("/auth/signup", requestBody);
-  };
+  async signUp(requestBody) {
+    return this.#api.post("/auth/signup", requestBody);
+  }
 
-  logIn = (requestBody) => {
-    // same as
-    // return axios.post("http://localhost:5005/auth/login");
-    return this.api.post("/auth/login", requestBody);
-  };
+  async logIn(requestBody) {
+    return this.#api.post("/auth/login", requestBody);
+  }
 
-  verifyToken = () => {
-    // same as
-    // return axios.get("http://localhost:5005/auth/verify");
-    return this.api.get("/auth/verify");
-  };
+  async verifyToken() {
+    return this.#api.get("/auth/verify");
+  }
 }
 
 const authService = new AuthService();
